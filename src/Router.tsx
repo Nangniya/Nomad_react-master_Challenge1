b9d3e065398e18a8de7b11b89a2cdaa7b11a47ab
nangniya <apinksky00@gmail.com>
2023-09-28 20:40:07 +0900

    fix: route

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,16 +2,14 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Coins from "./routes/Coins";
 import Coin from "./routes/Coin";
 
-interface IRouterProps {}
-
 function Router() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/:coinId">
+        <Route path="/Nomad_react-master_Challenge1/:coinId">
           <Coin />
         </Route>
-        <Route path="/">
+        <Route path="/Nomad_react-master_Challenge1">
           <Coins />
         </Route>
       </Switch>
